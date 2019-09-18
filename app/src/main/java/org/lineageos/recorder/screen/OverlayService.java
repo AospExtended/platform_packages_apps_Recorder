PixelExperience/packages_apps_Recorder

Improve floating window
* Add preference
* Add timer and stop button when recording

Change-Id: I351aaab37d7a2fb4b924472c880d690e721e3a4a

app/src/main/java/org/pixelexperience/recorder/screen/OverlayService.java
@@ -20,15 +20,22 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.Configuration;
 import android.os.IBinder;
+import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import org.pixelexperience.recorder.R;
 import org.pixelexperience.recorder.RecorderActivity;
 import org.pixelexperience.recorder.SettingsActivity;
 import org.pixelexperience.recorder.ui.OverlayLayer;
+import org.pixelexperience.recorder.utils.PreferenceUtils;
 import org.pixelexperience.recorder.utils.Utils;
 
 public class OverlayService extends Service {
@@ -46,25 +53,41 @@ public class OverlayService extends Service {
 
     private OverlayLayer mLayer;
 
+    private int mDensity;
+    private float mFontScale;
+    private int mUiMode;
+    private LocalBroadcastManager mLocalBroadcastManager;
+    private PreferenceUtils mPreferenceUtils;
+
+    private final BroadcastReceiver mRecordingStateChanged = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (Utils.ACTION_RECORDING_STATE_CHANGED.equals(intent.getAction())) {
+                if (Utils.isScreenRecording() && mPreferenceUtils.getShouldShowFloatingWindow()){
+                    mLayer.setIsRecording(true);
+                    stopForeground(true);
+                }else{
+                    onDestroy();
+                }
+            }else if (Utils.ACTION_RECORDING_TIME_TICK.equals(intent.getAction()) && mLayer != null) {
+                mLayer.updateTimerView(ScreencastService.sElapsedTimeInSeconds);
+            }
+        }
+    };
+
     @Override
     public int onStartCommand(Intent intent, int flags, int id) {
-        if (Utils.isScreenRecording()){
+        boolean shouldHideOverlay = Utils.isScreenRecording() && !mPreferenceUtils.getShouldShowFloatingWindow();
+        if (isRunning || shouldHideOverlay) {
             stopSelf();
+            return START_NOT_STICKY;
         }
-        mLayer = new OverlayLayer(this);
-        mLayer.setOnActionClickListener(() -> {
-            Intent intent_ = new Intent(this, StartScreenRecorder.class);
-            intent_.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent_);
-            onDestroy();
-        });
-        mLayer.setSettingsButtonOnClickListener(() -> {
-            Intent intent_ = new Intent(this, SettingsActivity.class);
-            intent_.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent_);
-            onDestroy();
-        });
-        mLayer.setCloseButtonOnClickListener(this::onDestroy);
+        Configuration configuration = getResources().getConfiguration();
+        mDensity = configuration.densityDpi;
+        mFontScale = configuration.fontScale;
+        mUiMode = configuration.uiMode;
+        createOverlayLayer();
+        registerReceiver();
 
         Notification notification = new NotificationCompat.Builder(
                 this, SCREENCAST_OVERLAY_NOTIFICATION_CHANNEL)
@@ -75,9 +98,39 @@ public class OverlayService extends Service {
                         new Intent(this, RecorderActivity.class), 0))
                 .build();
 
-        startForeground(FG_ID, notification);
+        if (Utils.isScreenRecording()) {
+            stopForeground(true);
+        }else{
+            startForeground(FG_ID, notification);
+        }
         isRunning = true;
         return START_NOT_STICKY;
+    }
+
+    private void createOverlayLayer() {
+        mLayer = new OverlayLayer(this);
+        mLayer.setOnActionClickListener(() -> {
+            if (Utils.isScreenRecording()){
+                Utils.setStatus(Utils.UiStatus.NOTHING, this);
+                startService(new Intent(ScreencastService.ACTION_STOP_SCREENCAST)
+                        .setClass(this, ScreencastService.class));
+                onDestroy();
+            }else {
+                registerReceiver();
+                Intent intent_ = new Intent(this, StartScreenRecorder.class);
+                intent_.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent_);
+            }
+        });
+        mLayer.setSettingsButtonOnClickListener(() -> {
+            Intent intent_ = new Intent(this, SettingsActivity.class);
+            intent_.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent_);
+            onDestroy();
+        });
+        mLayer.setCloseButtonOnClickListener(this::onDestroy);
+        mLayer.setIsRecording(Utils.isScreenRecording());
+        mLayer.updateTimerView(ScreencastService.sElapsedTimeInSeconds);
     }
 
     @Override
@@ -88,6 +141,9 @@ public class OverlayService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        mLocalBroadcastManager = LocalBroadcastManager.getInstance(this);
+        mPreferenceUtils = new PreferenceUtils(this);
 
         NotificationManager notificationManager = getSystemService(NotificationManager.class);
 
@@ -105,15 +161,54 @@ public class OverlayService extends Service {
         notificationManager.createNotificationChannel(notificationChannel);
     }
 
+    private void registerReceiver(){
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Utils.ACTION_RECORDING_STATE_CHANGED);
+        filter.addAction(Utils.ACTION_RECORDING_TIME_TICK);
+        mLocalBroadcastManager.registerReceiver(mRecordingStateChanged, filter);
+    }
+
+    private void unregisterReceiver(){
+        try{
+            mLocalBroadcastManager.unregisterReceiver(mRecordingStateChanged);
+        }catch (Exception e){
+        }
+    }
+
     @Override
     public void onDestroy() {
         if (mLayer != null) {
             mLayer.destroy();
             mLayer = null;
         }
+        unregisterReceiver();
 
         stopForeground(true);
         isRunning = false;
         super.onDestroy();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        boolean shouldRefresh = false;
+        if (mDensity != newConfig.densityDpi) {
+            mDensity = newConfig.densityDpi;
+            shouldRefresh = true;
+        }
+        if (mFontScale != newConfig.fontScale) {
+            mFontScale = newConfig.fontScale;
+            shouldRefresh = true;
+        }
+        if (mUiMode != newConfig.uiMode) {
+            mUiMode = newConfig.uiMode;
+            shouldRefresh = true;
+        }
+        if (shouldRefresh) {
+            if (mLayer != null) {
+                mLayer.destroy();
+                mLayer = null;
+            }
+            createOverlayLayer();
+        }
     }
 }

app/src/main/java/org/pixelexperience/recorder/utils/Utils.java
@@ -40,6 +40,7 @@ import java.lang.reflect.Method;
 
 public class Utils {
     public static final String ACTION_RECORDING_STATE_CHANGED = "org.pixelexperience.recorder.RECORDING_STATE_CHANGED";
+    public static final String ACTION_RECORDING_TIME_TICK = "org.pixelexperience.recorder.RECORDING_TIME_TICK";
     public static final String ACTION_HIDE_ACTIVITY = "org.pixelexperience.recorder.HIDE_ACTIVITY";
     public static final String PREF_RECORDING_NOTHING = "nothing";
     public static final String PREF_RECORDING_SCREEN = "screen";
@@ -152,6 +153,7 @@ public class Utils {
         // Stop overlay service if running
         if (OverlayService.isRunning) {
             context.stopService(new Intent(context, OverlayService.class));
+            OverlayService.isRunning = false;
         }
     }
 

AospExtended/platform_packages_apps_Recorder

Signed-off-by: DennySPB <dennyspb@gmail.com>
Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

app/src/main/java/org/lineageos/recorder/screen/OverlayService.java
@@ -20,15 +20,22 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.Configuration;
 import android.os.IBinder;
+import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import org.lineageos.recorder.R;
 import org.lineageos.recorder.RecorderActivity;
 import org.lineageos.recorder.SettingsActivity;
 import org.lineageos.recorder.ui.OverlayLayer;
+import org.lineageos.recorder.utils.PreferenceUtils;
 import org.lineageos.recorder.utils.Utils;
 
 public class OverlayService extends Service {
@@ -46,25 +53,41 @@ public class OverlayService extends Service {
 
     private OverlayLayer mLayer;
 
+    private int mDensity;
+    private float mFontScale;
+    private int mUiMode;
+    private LocalBroadcastManager mLocalBroadcastManager;
+    private PreferenceUtils mPreferenceUtils;
+
+    private final BroadcastReceiver mRecordingStateChanged = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (Utils.ACTION_RECORDING_STATE_CHANGED.equals(intent.getAction())) {
+                if (Utils.isScreenRecording() && mPreferenceUtils.getShouldShowFloatingWindow()){
+                    mLayer.setIsRecording(true);
+                    stopForeground(true);
+                }else{
+                    onDestroy();
+                }
+            }else if (Utils.ACTION_RECORDING_TIME_TICK.equals(intent.getAction()) && mLayer != null) {
+                mLayer.updateTimerView(ScreencastService.sElapsedTimeInSeconds);
+            }
+        }
+    };
+
     @Override
     public int onStartCommand(Intent intent, int flags, int id) {
-        if (Utils.isScreenRecording()){
+        boolean shouldHideOverlay = Utils.isScreenRecording() && !mPreferenceUtils.getShouldShowFloatingWindow();
+        if (isRunning || shouldHideOverlay) {
             stopSelf();
+            return START_NOT_STICKY;
         }
-        mLayer = new OverlayLayer(this);
-        mLayer.setOnActionClickListener(() -> {
-            Intent intent_ = new Intent(this, StartScreenRecorder.class);
-            intent_.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent_);
-            onDestroy();
-        });
-        mLayer.setSettingsButtonOnClickListener(() -> {
-            Intent intent_ = new Intent(this, SettingsActivity.class);
-            intent_.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent_);
-            onDestroy();
-        });
-        mLayer.setCloseButtonOnClickListener(this::onDestroy);
+        Configuration configuration = getResources().getConfiguration();
+        mDensity = configuration.densityDpi;
+        mFontScale = configuration.fontScale;
+        mUiMode = configuration.uiMode;
+        createOverlayLayer();
+        registerReceiver();
 
         Notification notification = new NotificationCompat.Builder(
                 this, SCREENCAST_OVERLAY_NOTIFICATION_CHANNEL)
@@ -75,9 +98,39 @@ public class OverlayService extends Service {
                         new Intent(this, RecorderActivity.class), 0))
                 .build();
 
-        startForeground(FG_ID, notification);
+        if (Utils.isScreenRecording()) {
+            stopForeground(true);
+        }else{
+            startForeground(FG_ID, notification);
+        }
         isRunning = true;
         return START_NOT_STICKY;
+    }
+
+    private void createOverlayLayer() {
+        mLayer = new OverlayLayer(this);
+        mLayer.setOnActionClickListener(() -> {
+            if (Utils.isScreenRecording()){
+                Utils.setStatus(Utils.UiStatus.NOTHING, this);
+                startService(new Intent(ScreencastService.ACTION_STOP_SCREENCAST)
+                        .setClass(this, ScreencastService.class));
+                onDestroy();
+            }else {
+                registerReceiver();
+                Intent intent_ = new Intent(this, StartScreenRecorder.class);
+                intent_.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent_);
+            }
+        });
+        mLayer.setSettingsButtonOnClickListener(() -> {
+            Intent intent_ = new Intent(this, SettingsActivity.class);
+            intent_.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent_);
+            onDestroy();
+        });
+        mLayer.setCloseButtonOnClickListener(this::onDestroy);
+        mLayer.setIsRecording(Utils.isScreenRecording());
+        mLayer.updateTimerView(ScreencastService.sElapsedTimeInSeconds);
     }
 
     @Override
@@ -88,6 +141,9 @@ public class OverlayService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        mLocalBroadcastManager = LocalBroadcastManager.getInstance(this);
+        mPreferenceUtils = new PreferenceUtils(this);
 
         NotificationManager notificationManager = getSystemService(NotificationManager.class);
 
@@ -105,15 +161,54 @@ public class OverlayService extends Service {
         notificationManager.createNotificationChannel(notificationChannel);
     }
 
+    private void registerReceiver(){
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Utils.ACTION_RECORDING_STATE_CHANGED);
+        filter.addAction(Utils.ACTION_RECORDING_TIME_TICK);
+        mLocalBroadcastManager.registerReceiver(mRecordingStateChanged, filter);
+    }
+
+    private void unregisterReceiver(){
+        try{
+            mLocalBroadcastManager.unregisterReceiver(mRecordingStateChanged);
+        }catch (Exception e){
+        }
+    }
+
     @Override
     public void onDestroy() {
         if (mLayer != null) {
             mLayer.destroy();
             mLayer = null;
         }
+        unregisterReceiver();
 
         stopForeground(true);
         isRunning = false;
         super.onDestroy();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        boolean shouldRefresh = false;
+        if (mDensity != newConfig.densityDpi) {
+            mDensity = newConfig.densityDpi;
+            shouldRefresh = true;
+        }
+        if (mFontScale != newConfig.fontScale) {
+            mFontScale = newConfig.fontScale;
+            shouldRefresh = true;
+        }
+        if (mUiMode != newConfig.uiMode) {
+            mUiMode = newConfig.uiMode;
+            shouldRefresh = true;
+        }
+        if (shouldRefresh) {
+            if (mLayer != null) {
+                mLayer.destroy();
+                mLayer = null;
+            }
+            createOverlayLayer();
+        }
     }
 }

app/src/main/java/org/lineageos/recorder/utils/Utils.java
@@ -39,6 +39,7 @@ import java.lang.reflect.Method;
 
 public class Utils {
     public static final String ACTION_RECORDING_STATE_CHANGED = "org.lineageos.recorder.RECORDING_STATE_CHANGED";
+    public static final String ACTION_RECORDING_TIME_TICK = "org.lineageos.recorder.RECORDING_TIME_TICK";
     public static final String ACTION_HIDE_ACTIVITY = "org.lineageos.recorder.HIDE_ACTIVITY";
     public static final String PREF_RECORDING_NOTHING = "nothing";
     public static final String PREF_RECORDING_SCREEN = "screen";
@@ -151,6 +152,7 @@ public class Utils {
         // Stop overlay service if running
         if (OverlayService.isRunning) {
             context.stopService(new Intent(context, OverlayService.class));
+            OverlayService.isRunning = false;
         }
     }
 