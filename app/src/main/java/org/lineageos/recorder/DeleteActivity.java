PixelExperience/packages_apps_Recorder

Refactor settings UI

app/src/main/java/org/pixelexperience/recorder/DeleteActivity.java
@@ -1,0 +1,24 @@
+package org.pixelexperience.recorder;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.os.Bundle;
+
+import org.pixelexperience.recorder.utils.LastRecordHelper;
+
+public class DeleteActivity extends Activity {
+    public static final String EXTRA_LAST_SOUND = "lastSoundItem";
+
+    @Override
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        deleteLastItem(getIntent().getBooleanExtra(EXTRA_LAST_SOUND, false));
+    }
+
+    private void deleteLastItem(boolean isSound) {
+        String path = LastRecordHelper.getLastItemPath(this, isSound);
+        AlertDialog dialog = LastRecordHelper.deleteFile(this, path, isSound);
+        dialog.setOnDismissListener(d -> finish());
+        dialog.show();
+    }
+}

app/src/main/java/org/pixelexperience/recorder/SettingsActivity.java
@@ -1,0 +1,96 @@
+package org.pixelexperience.recorder;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.ListPreference;
+import androidx.preference.PreferenceFragmentCompat;
+
+import org.pixelexperience.recorder.utils.PermissionUtils;
+import org.pixelexperience.recorder.utils.PreferenceUtils;
+import org.pixelexperience.recorder.utils.Utils;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    private static final String TAG = "SettingsActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings, new SettingsFragment())
+                .commit();
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {// app icon in action bar clicked; go home
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(this, RecorderActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        super.onBackPressed();
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        private static final int REQUEST_RECORD_AUDIO_PERMS = 213;
+        private ListPreference mAudioSource;
+        private PreferenceUtils mPreferenceUtils;
+
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.settings, rootKey);
+            mPreferenceUtils = new PreferenceUtils(getContext());
+            mAudioSource = findPreference(PreferenceUtils.PREF_AUDIO_RECORDING_TYPE);
+            mAudioSource.setOnPreferenceChangeListener((preference, newValue) -> {
+                int value = Integer.valueOf((String) newValue);
+                mPreferenceUtils.setAudioRecordingType(value);
+                if (!PermissionUtils.hasAudioPermission(getContext()) && value != PreferenceUtils.PREF_AUDIO_RECORDING_TYPE_DISABLED) {
+                    askAudioPermission();
+                }
+                return true;
+            });
+            mAudioSource.setValueIndex(mPreferenceUtils.getAudioRecordingType());
+            if (Utils.isScreenRecording()) {
+                mAudioSource.setEnabled(false);
+            }
+        }
+
+        private void askAudioPermission() {
+            requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO},
+                    REQUEST_RECORD_AUDIO_PERMS);
+        }
+
+        @Override
+        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                               @NonNull int[] results) {
+            if (requestCode != REQUEST_RECORD_AUDIO_PERMS) {
+                return;
+            }
+
+            if (!PermissionUtils.hasAudioPermission(getContext())) {
+                mPreferenceUtils.setAudioRecordingType(PreferenceUtils.PREF_AUDIO_RECORDING_TYPE_DISABLED);
+                mAudioSource.setValueIndex(PreferenceUtils.PREF_AUDIO_RECORDING_TYPE_DISABLED);
+            }
+        }
+    }
+
+}

app/src/main/java/org/pixelexperience/recorder/screen/OverlayService.java
@@ -27,8 +27,8 @@ import androidx.core.app.NotificationCompat;
 
 import org.pixelexperience.recorder.R;
 import org.pixelexperience.recorder.RecorderActivity;
+import org.pixelexperience.recorder.SettingsActivity;
 import org.pixelexperience.recorder.ui.OverlayLayer;
-import org.pixelexperience.recorder.utils.Utils;
 
 public class OverlayService extends Service {
 
@@ -55,9 +55,8 @@ public class OverlayService extends Service {
             onDestroy();
         });
         mLayer.setSettingsButtonOnClickListener(() -> {
-            Intent intent_ = new Intent(this, RecorderActivity.class);
+            Intent intent_ = new Intent(this, SettingsActivity.class);
             intent_.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent_.putExtra(RecorderActivity.EXTRA_UI_TYPE, Utils.SCREEN_PREFS);
             startActivity(intent_);
             onDestroy();
         });

app/src/main/java/org/pixelexperience/recorder/utils/LastRecordHelper.java
@@ -15,15 +15,16 @@
  */
 package org.pixelexperience.recorder.utils;
 
+import android.app.AlertDialog;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
-import androidx.core.content.FileProvider;
-import androidx.appcompat.app.AlertDialog;
 
-import org.pixelexperience.recorder.DialogActivity;
+import androidx.core.content.FileProvider;
+
+import org.pixelexperience.recorder.DeleteActivity;
 import org.pixelexperience.recorder.R;
 import org.pixelexperience.recorder.screen.ScreencastService;
 import org.pixelexperience.recorder.sounds.SoundRecorderService;
@@ -42,7 +43,7 @@ public class LastRecordHelper {
     }
 
     public static AlertDialog deleteFile(Context context, final String path, boolean isSound) {
-        return new AlertDialog.Builder(context)
+        return new AlertDialog.Builder(context, android.R.style.Theme_DeviceDefault_Dialog)
                 .setTitle(R.string.delete_title)
                 .setMessage(context.getString(R.string.delete_message, path))
                 .setPositiveButton(R.string.delete, (dialog, which) -> {
@@ -88,12 +89,8 @@ public class LastRecordHelper {
     }
 
     public static Intent getDeleteIntent(Context context, boolean isSound) {
-        Intent intent = new Intent(context, DialogActivity.class);
-        intent.putExtra(DialogActivity.EXTRA_TITLE,
-                isSound ? R.string.sound_last_title : R.string.screen_last_title);
-        intent.putExtra(DialogActivity.EXTRA_LAST_SCREEN, !isSound);
-        intent.putExtra(DialogActivity.EXTRA_LAST_SOUND, isSound);
-        intent.putExtra(DialogActivity.EXTRA_DELETE_LAST_RECORDING, true);
+        Intent intent = new Intent(context, DeleteActivity.class);
+        intent.putExtra(DeleteActivity.EXTRA_LAST_SOUND, isSound);
         return intent;
     }
 
@@ -111,25 +108,4 @@ public class LastRecordHelper {
         return prefs.getString(isSound ? KEY_LAST_SOUND : KEY_LAST_SCREEN, null);
     }
 
-    private static long getLastItemDuration(Context context, boolean isSound) {
-        SharedPreferences prefs = context.getSharedPreferences(PREFS, 0);
-        return prefs.getLong(isSound ? KEY_LAST_SOUND_TIME : KEY_LAST_SCREEN_TIME, -1);
-    }
-
-    private static String getLastItemDate(Context context, boolean isSound) {
-        String path = getLastItemPath(context, isSound);
-        String[] pathParts = path.split("/");
-        String[] date = pathParts[pathParts.length - 1]
-                .replace(isSound ? ".wav" : ".mp4", "")
-                .replace(isSound ? "SoundRecord" : "ScreenRecord", "")
-                .split("-");
-        return context.getString(R.string.date_format, date[1], date[2], date[3],
-                date[4], date[5]);
-    }
-
-    public static String getLastItemDescription(Context context, boolean isSound) {
-        return context.getString(R.string.screen_last_message,
-                getLastItemDate(context, isSound),
-                getLastItemDuration(context, isSound) / 1000);
-    }
 }

AospExtended/platform_packages_apps_Recorder

Change-Id: I3bead059dc9924335bef42feb98f501929fa49b4
Signed-off-by: DennySPB <dennyspb@gmail.com>
Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

app/src/main/java/org/lineageos/recorder/DeleteActivity.java
@@ -1,0 +1,24 @@
+package org.lineageos.recorder;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.os.Bundle;
+
+import org.lineageos.recorder.utils.LastRecordHelper;
+
+public class DeleteActivity extends Activity {
+    public static final String EXTRA_LAST_SOUND = "lastSoundItem";
+
+    @Override
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        deleteLastItem(getIntent().getBooleanExtra(EXTRA_LAST_SOUND, false));
+    }
+
+    private void deleteLastItem(boolean isSound) {
+        String path = LastRecordHelper.getLastItemPath(this, isSound);
+        AlertDialog dialog = LastRecordHelper.deleteFile(this, path, isSound);
+        dialog.setOnDismissListener(d -> finish());
+        dialog.show();
+    }
+}

app/src/main/java/org/lineageos/recorder/SettingsActivity.java
@@ -1,0 +1,96 @@
+package org.lineageos.recorder;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.ListPreference;
+import androidx.preference.PreferenceFragmentCompat;
+
+import org.lineageos.recorder.utils.PermissionUtils;
+import org.lineageos.recorder.utils.PreferenceUtils;
+import org.lineageos.recorder.utils.Utils;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    private static final String TAG = "SettingsActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings, new SettingsFragment())
+                .commit();
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {// app icon in action bar clicked; go home
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(this, RecorderActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        super.onBackPressed();
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        private static final int REQUEST_RECORD_AUDIO_PERMS = 213;
+        private ListPreference mAudioSource;
+        private PreferenceUtils mPreferenceUtils;
+
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.settings, rootKey);
+            mPreferenceUtils = new PreferenceUtils(getContext());
+            mAudioSource = findPreference(PreferenceUtils.PREF_AUDIO_RECORDING_TYPE);
+            mAudioSource.setOnPreferenceChangeListener((preference, newValue) -> {
+                int value = Integer.valueOf((String) newValue);
+                mPreferenceUtils.setAudioRecordingType(value);
+                if (!PermissionUtils.hasAudioPermission(getContext()) && value != PreferenceUtils.PREF_AUDIO_RECORDING_TYPE_DISABLED) {
+                    askAudioPermission();
+                }
+                return true;
+            });
+            mAudioSource.setValueIndex(mPreferenceUtils.getAudioRecordingType());
+            if (Utils.isScreenRecording()) {
+                mAudioSource.setEnabled(false);
+            }
+        }
+
+        private void askAudioPermission() {
+            requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO},
+                    REQUEST_RECORD_AUDIO_PERMS);
+        }
+
+        @Override
+        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                               @NonNull int[] results) {
+            if (requestCode != REQUEST_RECORD_AUDIO_PERMS) {
+                return;
+            }
+
+            if (!PermissionUtils.hasAudioPermission(getContext())) {
+                mPreferenceUtils.setAudioRecordingType(PreferenceUtils.PREF_AUDIO_RECORDING_TYPE_DISABLED);
+                mAudioSource.setValueIndex(PreferenceUtils.PREF_AUDIO_RECORDING_TYPE_DISABLED);
+            }
+        }
+    }
+
+}

app/src/main/java/org/lineageos/recorder/screen/OverlayService.java
@@ -27,8 +27,8 @@ import androidx.core.app.NotificationCompat;
 
 import org.lineageos.recorder.R;
 import org.lineageos.recorder.RecorderActivity;
+import org.lineageos.recorder.SettingsActivity;
 import org.lineageos.recorder.ui.OverlayLayer;
-import org.lineageos.recorder.utils.Utils;
 
 public class OverlayService extends Service {
 
@@ -55,9 +55,8 @@ public class OverlayService extends Service {
             onDestroy();
         });
         mLayer.setSettingsButtonOnClickListener(() -> {
-            Intent intent_ = new Intent(this, RecorderActivity.class);
+            Intent intent_ = new Intent(this, SettingsActivity.class);
             intent_.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent_.putExtra(RecorderActivity.EXTRA_UI_TYPE, Utils.SCREEN_PREFS);
             startActivity(intent_);
             onDestroy();
         });

app/src/main/java/org/lineageos/recorder/utils/LastRecordHelper.java
@@ -15,15 +15,16 @@
  */
 package org.lineageos.recorder.utils;
 
+import android.app.AlertDialog;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
-import androidx.core.content.FileProvider;
-import androidx.appcompat.app.AlertDialog;
 
-import org.lineageos.recorder.DialogActivity;
+import androidx.core.content.FileProvider;
+
+import org.lineageos.recorder.DeleteActivity;
 import org.lineageos.recorder.R;
 import org.lineageos.recorder.screen.ScreencastService;
 import org.lineageos.recorder.sounds.SoundRecorderService;
@@ -42,7 +43,7 @@ public class LastRecordHelper {
     }
 
     public static AlertDialog deleteFile(Context context, final String path, boolean isSound) {
-        return new AlertDialog.Builder(context)
+        return new AlertDialog.Builder(context, android.R.style.Theme_DeviceDefault_Dialog)
                 .setTitle(R.string.delete_title)
                 .setMessage(context.getString(R.string.delete_message, path))
                 .setPositiveButton(R.string.delete, (dialog, which) -> {
@@ -88,12 +89,8 @@ public class LastRecordHelper {
     }
 
     public static Intent getDeleteIntent(Context context, boolean isSound) {
-        Intent intent = new Intent(context, DialogActivity.class);
-        intent.putExtra(DialogActivity.EXTRA_TITLE,
-                isSound ? R.string.sound_last_title : R.string.screen_last_title);
-        intent.putExtra(DialogActivity.EXTRA_LAST_SCREEN, !isSound);
-        intent.putExtra(DialogActivity.EXTRA_LAST_SOUND, isSound);
-        intent.putExtra(DialogActivity.EXTRA_DELETE_LAST_RECORDING, true);
+        Intent intent = new Intent(context, DeleteActivity.class);
+        intent.putExtra(DeleteActivity.EXTRA_LAST_SOUND, isSound);
         return intent;
     }
 
@@ -111,25 +108,4 @@ public class LastRecordHelper {
         return prefs.getString(isSound ? KEY_LAST_SOUND : KEY_LAST_SCREEN, null);
     }
 
-    private static long getLastItemDuration(Context context, boolean isSound) {
-        SharedPreferences prefs = context.getSharedPreferences(PREFS, 0);
-        return prefs.getLong(isSound ? KEY_LAST_SOUND_TIME : KEY_LAST_SCREEN_TIME, -1);
-    }
-
-    private static String getLastItemDate(Context context, boolean isSound) {
-        String path = getLastItemPath(context, isSound);
-        String[] pathParts = path.split("/");
-        String[] date = pathParts[pathParts.length - 1]
-                .replace(isSound ? ".wav" : ".mp4", "")
-                .replace(isSound ? "SoundRecord" : "ScreenRecord", "")
-                .split("-");
-        return context.getString(R.string.date_format, date[1], date[2], date[3],
-                date[4], date[5]);
-    }
-
-    public static String getLastItemDescription(Context context, boolean isSound) {
-        return context.getString(R.string.screen_last_message,
-                getLastItemDate(context, isSound),
-                getLastItemDuration(context, isSound) / 1000);
-    }
 }