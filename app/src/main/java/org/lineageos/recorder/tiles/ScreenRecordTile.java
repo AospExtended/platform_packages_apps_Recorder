PixelExperience/packages_apps_Recorder

Implement qs tiles and refactor code

app/src/main/java/org/pixelexperience/recorder/tiles/ScreenRecordTile.java
@@ -1,0 +1,101 @@
+package org.pixelexperience.recorder.tiles;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import org.pixelexperience.recorder.R;
+import org.pixelexperience.recorder.RecorderActivity;
+import org.pixelexperience.recorder.screen.OverlayService;
+import org.pixelexperience.recorder.screen.ScreencastService;
+import org.pixelexperience.recorder.utils.GlobalSettings;
+import org.pixelexperience.recorder.utils.PermissionUtils;
+import org.pixelexperience.recorder.utils.Utils;
+
+public class ScreenRecordTile extends TileService {
+
+    private final BroadcastReceiver mRecordingStateChanged = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (Utils.ACTION_RECORDING_STATE_CHANGED.equals(intent.getAction())) {
+                updateTile();
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onClick() {
+        if (Utils.isScreenRecording()) {
+            Utils.collapseStatusBar(this);
+            Utils.setStatus(Utils.UiStatus.NOTHING, this);
+            startService(new Intent(ScreencastService.ACTION_STOP_SCREENCAST)
+                    .setClass(this, ScreencastService.class));
+        } else if (hasPerms()) {
+            Utils.collapseStatusBar(this);
+            LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(Utils.ACTION_HIDE_ACTIVITY));
+            Utils.stopOverlayService(this);
+            Intent intent = new Intent(this, OverlayService.class);
+            startService(intent);
+        } else {
+            Intent intent = new Intent(this, RecorderActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.putExtra(RecorderActivity.EXTRA_UI_TYPE, Utils.UiStatus.SCREEN.toString());
+            startActivityAndCollapse(intent);
+        }
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        updateTile();
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRecordingStateChanged,
+                new IntentFilter(Utils.ACTION_RECORDING_STATE_CHANGED));
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRecordingStateChanged);
+    }
+
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+        updateTile();
+    }
+
+    private void updateTile() {
+        Tile qsTile = getQsTile();
+        if (GlobalSettings.sRecordingStatus.equals(Utils.PREF_RECORDING_SOUND)) {
+            qsTile.setState(Tile.STATE_UNAVAILABLE);
+        } else {
+            qsTile.setState(GlobalSettings.sRecordingStatus.equals(Utils.PREF_RECORDING_SCREEN)
+                    ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        }
+        qsTile.setLabel(getString(Utils.isScreenRecording() ?
+                R.string.screen_recording_message : R.string.screen_notification_title));
+        qsTile.updateTile();
+    }
+
+
+    private boolean hasPerms() {
+        if (!PermissionUtils.hasDrawOverOtherAppsPermission(this)) {
+            return false;
+        }
+
+        if (!PermissionUtils.hasStoragePermission(this)) {
+            return false;
+        }
+        return true;
+    }
+}

app/src/main/java/org/pixelexperience/recorder/tiles/SoundRecordTile.java
@@ -1,0 +1,68 @@
+package org.pixelexperience.recorder.tiles;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import org.pixelexperience.recorder.R;
+import org.pixelexperience.recorder.RecorderActivity;
+import org.pixelexperience.recorder.utils.GlobalSettings;
+import org.pixelexperience.recorder.utils.Utils;
+
+public class SoundRecordTile extends TileService {
+    private final BroadcastReceiver mRecordingStateChanged = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (Utils.ACTION_RECORDING_STATE_CHANGED.equals(intent.getAction())) {
+                updateTile();
+            }
+        }
+    };
+
+    @Override
+    public void onClick() {
+        Intent intent = new Intent(this, RecorderActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra(RecorderActivity.EXTRA_UI_TYPE, Utils.UiStatus.SOUND.toString());
+        startActivityAndCollapse(intent);
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        updateTile();
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRecordingStateChanged,
+                new IntentFilter(Utils.ACTION_RECORDING_STATE_CHANGED));
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRecordingStateChanged);
+    }
+
+    @Override
+    public void onTileAdded(){
+        super.onTileAdded();
+        updateTile();
+    }
+
+    private void updateTile(){
+        Tile qsTile = getQsTile();
+        if (GlobalSettings.sRecordingStatus.equals(Utils.PREF_RECORDING_SCREEN)){
+            qsTile.setState(Tile.STATE_UNAVAILABLE);
+        }else {
+            qsTile.setState(GlobalSettings.sRecordingStatus.equals(Utils.PREF_RECORDING_SOUND)
+                    ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        }
+        qsTile.setLabel(getString(Utils.isSoundRecording() ?
+                R.string.sound_recording_title_working : R.string.sound_notification_title));
+        qsTile.updateTile();
+    }
+
+}

app/src/main/java/org/pixelexperience/recorder/utils/PermissionUtils.java
@@ -1,0 +1,35 @@
+package org.pixelexperience.recorder.utils;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.provider.Settings;
+
+public class PermissionUtils {
+    public static boolean hasStoragePermission(Context context) {
+        int result = context.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public static boolean hasAudioPermission(Context context) {
+        int result = context.checkSelfPermission(Manifest.permission.RECORD_AUDIO);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public static boolean hasPhoneReaderPermission(Context context) {
+        int result = context.checkSelfPermission(Manifest.permission.READ_PHONE_STATE);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public static boolean hasDrawOverOtherAppsPermission(Context context) {
+        return Settings.canDrawOverlays(context);
+    }
+
+    public static boolean hasAllAudioRecorderPermissions(Context context) {
+        return hasStoragePermission(context) && hasAudioPermission(context) && hasPhoneReaderPermission(context);
+    }
+
+    public static boolean hasAllScreenRecorderPermissions(Context context) {
+        return hasStoragePermission(context);
+    }
+}

app/src/main/java/org/pixelexperience/recorder/utils/Utils.java
@@ -18,23 +18,29 @@ package org.pixelexperience.recorder.utils;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothProfile;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.media.AudioSystem;
 import android.util.DisplayMetrics;
 import android.widget.Toast;
 
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
 import org.pixelexperience.recorder.R;
+import org.pixelexperience.recorder.screen.OverlayService;
 
 import java.io.Closeable;
 import java.io.IOException;
 
 public class Utils {
     public static final String PREFS = "preferences";
-    public static final String KEY_RECORDING = "recording";
+    public static final String SCREEN_PREFS = "screen_preferences";
+    public static final String ACTION_RECORDING_STATE_CHANGED = "org.pixelexperience.recorder.RECORDING_STATE_CHANGED";
+    public static final String ACTION_HIDE_ACTIVITY = "org.pixelexperience.recorder.HIDE_ACTIVITY";
     public static final String PREF_RECORDING_NOTHING = "nothing";
     public static final String PREF_RECORDING_SCREEN = "screen";
-    private static final String PREF_RECORDING_SOUND = "sound";
+    public static final String PREF_RECORDING_SOUND = "sound";
     public static final String PREF_AUDIO_RECORDING_TYPE = "audio_recording_type";
     public static final int PREF_AUDIO_RECORDING_TYPE_DISABLED = 0;
     public static final int PREF_AUDIO_RECORDING_TYPE_INTERNAL = 1;
@@ -44,11 +50,11 @@ public class Utils {
     private Utils() {
     }
 
-    public static boolean isWifiDisplaySessionRunning(){
+    public static boolean isWifiDisplaySessionRunning() {
         return (AudioSystem.getDevicesForStream(AudioSystem.STREAM_MUSIC) & AudioSystem.DEVICE_OUT_PROXY) != 0;
     }
 
-    public static boolean isRoutedToSubmix(){
+    public static boolean isRoutedToSubmix() {
         return (AudioSystem.getDevicesForStream(AudioSystem.STREAM_MUSIC) & AudioSystem.DEVICE_OUT_REMOTE_SUBMIX) != 0;
     }
 
@@ -62,8 +68,8 @@ public class Utils {
         return a2dp == BluetoothProfile.STATE_CONNECTED || headset == BluetoothProfile.STATE_CONNECTED;
     }
 
-    public static boolean isRoutedOnlyToSpeakerOrHeadset(){
-        if (isBluetoothHeadsetConnected()){
+    public static boolean isRoutedOnlyToSpeakerOrHeadset() {
+        if (isBluetoothHeadsetConnected()) {
             return false;
         }
         int devices = AudioSystem.getDevicesForStream(AudioSystem.STREAM_MUSIC);
@@ -73,17 +79,17 @@ public class Utils {
                 devices == AudioSystem.DEVICE_OUT_USB_HEADSET);
     }
 
-    public static boolean isInternalAudioRecordingAllowed(Context context, boolean checkSubmix){
-        if (isWifiDisplaySessionRunning()){
-            Toast.makeText(context, R.string.screen_audio_recording_disabled_wfd,Toast.LENGTH_LONG).show();
+    public static boolean isInternalAudioRecordingAllowed(Context context, boolean checkSubmix) {
+        if (isWifiDisplaySessionRunning()) {
+            Toast.makeText(context, R.string.screen_audio_recording_disabled_wfd, Toast.LENGTH_LONG).show();
             return false;
         }
-        if (checkSubmix && isRoutedToSubmix()){
-            Toast.makeText(context, R.string.screen_audio_recording_disabled_others_apps,Toast.LENGTH_LONG).show();
+        if (checkSubmix && isRoutedToSubmix()) {
+            Toast.makeText(context, R.string.screen_audio_recording_disabled_others_apps, Toast.LENGTH_LONG).show();
             return false;
         }
-        if (!isRoutedOnlyToSpeakerOrHeadset()){
-            Toast.makeText(context, R.string.screen_audio_recording_not_allowed,Toast.LENGTH_LONG).show();
+        if (!isRoutedOnlyToSpeakerOrHeadset()) {
+            Toast.makeText(context, R.string.screen_audio_recording_not_allowed, Toast.LENGTH_LONG).show();
             return false;
         }
         return true;
@@ -93,18 +99,19 @@ public class Utils {
         return GlobalSettings.sRecordingStatus;
     }
 
-    public static void setStatus(UiStatus status) {
+    public static void setStatus(UiStatus status, Context context) {
         if (status.equals(UiStatus.SOUND)) {
-            setStatus(PREF_RECORDING_SOUND);
+            setStatus(PREF_RECORDING_SOUND, context);
         } else if (status.equals(UiStatus.SCREEN)) {
-            setStatus(PREF_RECORDING_SCREEN);
+            setStatus(PREF_RECORDING_SCREEN, context);
         } else {
-            setStatus(PREF_RECORDING_NOTHING);
+            setStatus(PREF_RECORDING_NOTHING, context);
         }
     }
 
-    public static void setStatus(String status) {
+    public static void setStatus(String status, Context context) {
         GlobalSettings.sRecordingStatus = status;
+        LocalBroadcastManager.getInstance(context).sendBroadcast(new Intent(Utils.ACTION_RECORDING_STATE_CHANGED));
     }
 
     public static boolean isRecording() {
@@ -136,6 +143,18 @@ public class Utils {
     private static int getDarkenedColorValue(int value) {
         float dark = 0.8f; // -20% lightness
         return Math.min(Math.round(value * dark), 255);
+    }
+
+    public static int getAudioRecordingType(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences(Utils.PREFS, 0);
+        return prefs.getInt(Utils.PREF_AUDIO_RECORDING_TYPE, Utils.PREF_AUDIO_RECORDING_TYPE_DEFAULT);
+    }
+
+    public static void stopOverlayService(Context context) {
+        // Stop overlay service if running
+        if (OverlayService.isRunning) {
+            context.stopService(new Intent(context, OverlayService.class));
+        }
     }
 
     /**
@@ -176,6 +195,10 @@ public class Utils {
         NOTHING,
         SOUND,
         SCREEN
+    }
+
+    public static void collapseStatusBar(Context context) {
+        context.sendBroadcast(new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS));
     }
 
 }

AospExtended/platform_packages_apps_Recorder

Signed-off-by: DennySPB <dennyspb@gmail.com>
Change-Id: I0cd346b90ed2eb44f6c0012696dd5045ea2314a2
Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

app/src/main/java/org/lineageos/recorder/tiles/ScreenRecordTile.java
@@ -1,0 +1,101 @@
+package org.lineageos.recorder.tiles;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import org.lineageos.recorder.R;
+import org.lineageos.recorder.RecorderActivity;
+import org.lineageos.recorder.screen.OverlayService;
+import org.lineageos.recorder.screen.ScreencastService;
+import org.lineageos.recorder.utils.GlobalSettings;
+import org.lineageos.recorder.utils.PermissionUtils;
+import org.lineageos.recorder.utils.Utils;
+
+public class ScreenRecordTile extends TileService {
+
+    private final BroadcastReceiver mRecordingStateChanged = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (Utils.ACTION_RECORDING_STATE_CHANGED.equals(intent.getAction())) {
+                updateTile();
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onClick() {
+        if (Utils.isScreenRecording()) {
+            Utils.collapseStatusBar(this);
+            Utils.setStatus(Utils.UiStatus.NOTHING, this);
+            startService(new Intent(ScreencastService.ACTION_STOP_SCREENCAST)
+                    .setClass(this, ScreencastService.class));
+        } else if (hasPerms()) {
+            Utils.collapseStatusBar(this);
+            LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(Utils.ACTION_HIDE_ACTIVITY));
+            Utils.stopOverlayService(this);
+            Intent intent = new Intent(this, OverlayService.class);
+            startService(intent);
+        } else {
+            Intent intent = new Intent(this, RecorderActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.putExtra(RecorderActivity.EXTRA_UI_TYPE, Utils.UiStatus.SCREEN.toString());
+            startActivityAndCollapse(intent);
+        }
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        updateTile();
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRecordingStateChanged,
+                new IntentFilter(Utils.ACTION_RECORDING_STATE_CHANGED));
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRecordingStateChanged);
+    }
+
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+        updateTile();
+    }
+
+    private void updateTile() {
+        Tile qsTile = getQsTile();
+        if (GlobalSettings.sRecordingStatus.equals(Utils.PREF_RECORDING_SOUND)) {
+            qsTile.setState(Tile.STATE_UNAVAILABLE);
+        } else {
+            qsTile.setState(GlobalSettings.sRecordingStatus.equals(Utils.PREF_RECORDING_SCREEN)
+                    ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        }
+        qsTile.setLabel(getString(Utils.isScreenRecording() ?
+                R.string.screen_recording_message : R.string.screen_notification_title));
+        qsTile.updateTile();
+    }
+
+
+    private boolean hasPerms() {
+        if (!PermissionUtils.hasDrawOverOtherAppsPermission(this)) {
+            return false;
+        }
+
+        if (!PermissionUtils.hasStoragePermission(this)) {
+            return false;
+        }
+        return true;
+    }
+}

app/src/main/java/org/lineageos/recorder/tiles/SoundRecordTile.java
@@ -1,0 +1,68 @@
+package org.lineageos.recorder.tiles;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import org.lineageos.recorder.R;
+import org.lineageos.recorder.RecorderActivity;
+import org.lineageos.recorder.utils.GlobalSettings;
+import org.lineageos.recorder.utils.Utils;
+
+public class SoundRecordTile extends TileService {
+    private final BroadcastReceiver mRecordingStateChanged = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (Utils.ACTION_RECORDING_STATE_CHANGED.equals(intent.getAction())) {
+                updateTile();
+            }
+        }
+    };
+
+    @Override
+    public void onClick() {
+        Intent intent = new Intent(this, RecorderActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra(RecorderActivity.EXTRA_UI_TYPE, Utils.UiStatus.SOUND.toString());
+        startActivityAndCollapse(intent);
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        updateTile();
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRecordingStateChanged,
+                new IntentFilter(Utils.ACTION_RECORDING_STATE_CHANGED));
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRecordingStateChanged);
+    }
+
+    @Override
+    public void onTileAdded(){
+        super.onTileAdded();
+        updateTile();
+    }
+
+    private void updateTile(){
+        Tile qsTile = getQsTile();
+        if (GlobalSettings.sRecordingStatus.equals(Utils.PREF_RECORDING_SCREEN)){
+            qsTile.setState(Tile.STATE_UNAVAILABLE);
+        }else {
+            qsTile.setState(GlobalSettings.sRecordingStatus.equals(Utils.PREF_RECORDING_SOUND)
+                    ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        }
+        qsTile.setLabel(getString(Utils.isSoundRecording() ?
+                R.string.sound_recording_title_working : R.string.sound_notification_title));
+        qsTile.updateTile();
+    }
+
+}

app/src/main/java/org/lineageos/recorder/utils/PermissionUtils.java
@@ -1,0 +1,35 @@
+package org.lineageos.recorder.utils;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.provider.Settings;
+
+public class PermissionUtils {
+    public static boolean hasStoragePermission(Context context) {
+        int result = context.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public static boolean hasAudioPermission(Context context) {
+        int result = context.checkSelfPermission(Manifest.permission.RECORD_AUDIO);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public static boolean hasPhoneReaderPermission(Context context) {
+        int result = context.checkSelfPermission(Manifest.permission.READ_PHONE_STATE);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public static boolean hasDrawOverOtherAppsPermission(Context context) {
+        return Settings.canDrawOverlays(context);
+    }
+
+    public static boolean hasAllAudioRecorderPermissions(Context context) {
+        return hasStoragePermission(context) && hasAudioPermission(context) && hasPhoneReaderPermission(context);
+    }
+
+    public static boolean hasAllScreenRecorderPermissions(Context context) {
+        return hasStoragePermission(context);
+    }
+}

app/src/main/java/org/lineageos/recorder/utils/Utils.java
@@ -18,6 +18,7 @@ package org.lineageos.recorder.utils;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothProfile;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.media.AudioSystem;
@@ -25,16 +26,20 @@ import android.util.DisplayMetrics;
 import android.widget.Toast;
 
 import org.lineageos.recorder.R;
+import org.lineageos.recorder.screen.OverlayService;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import java.io.Closeable;
 import java.io.IOException;
 
 public class Utils {
     public static final String PREFS = "preferences";
-    public static final String KEY_RECORDING = "recording";
+    public static final String SCREEN_PREFS = "screen_preferences";
+    public static final String ACTION_RECORDING_STATE_CHANGED = "org.lineageos.recorder.RECORDING_STATE_CHANGED";
+    public static final String ACTION_HIDE_ACTIVITY = "org.lineageos.recorder.HIDE_ACTIVITY";
     public static final String PREF_RECORDING_NOTHING = "nothing";
     public static final String PREF_RECORDING_SCREEN = "screen";
-    private static final String PREF_RECORDING_SOUND = "sound";
+    public static final String PREF_RECORDING_SOUND = "sound";
     public static final String PREF_AUDIO_RECORDING_TYPE = "audio_recording_type";
     public static final int PREF_AUDIO_RECORDING_TYPE_DISABLED = 0;
     public static final int PREF_AUDIO_RECORDING_TYPE_INTERNAL = 1;
@@ -44,11 +49,11 @@ public class Utils {
     private Utils() {
     }
 
-    public static boolean isWifiDisplaySessionRunning(){
+    public static boolean isWifiDisplaySessionRunning() {
         return (AudioSystem.getDevicesForStream(AudioSystem.STREAM_MUSIC) & AudioSystem.DEVICE_OUT_PROXY) != 0;
     }
 
-    public static boolean isRoutedToSubmix(){
+    public static boolean isRoutedToSubmix() {
         return (AudioSystem.getDevicesForStream(AudioSystem.STREAM_MUSIC) & AudioSystem.DEVICE_OUT_REMOTE_SUBMIX) != 0;
     }
 
@@ -62,8 +67,8 @@ public class Utils {
         return a2dp == BluetoothProfile.STATE_CONNECTED || headset == BluetoothProfile.STATE_CONNECTED;
     }
 
-    public static boolean isRoutedOnlyToSpeakerOrHeadset(){
-        if (isBluetoothHeadsetConnected()){
+    public static boolean isRoutedOnlyToSpeakerOrHeadset() {
+        if (isBluetoothHeadsetConnected()) {
             return false;
         }
         int devices = AudioSystem.getDevicesForStream(AudioSystem.STREAM_MUSIC);
@@ -73,17 +78,17 @@ public class Utils {
                 devices == AudioSystem.DEVICE_OUT_USB_HEADSET);
     }
 
-    public static boolean isInternalAudioRecordingAllowed(Context context, boolean checkSubmix){
-        if (isWifiDisplaySessionRunning()){
-            Toast.makeText(context, R.string.screen_audio_recording_disabled_wfd,Toast.LENGTH_LONG).show();
+    public static boolean isInternalAudioRecordingAllowed(Context context, boolean checkSubmix) {
+        if (isWifiDisplaySessionRunning()) {
+            Toast.makeText(context, R.string.screen_audio_recording_disabled_wfd, Toast.LENGTH_LONG).show();
             return false;
         }
-        if (checkSubmix && isRoutedToSubmix()){
-            Toast.makeText(context, R.string.screen_audio_recording_disabled_others_apps,Toast.LENGTH_LONG).show();
+        if (checkSubmix && isRoutedToSubmix()) {
+            Toast.makeText(context, R.string.screen_audio_recording_disabled_others_apps, Toast.LENGTH_LONG).show();
             return false;
         }
-        if (!isRoutedOnlyToSpeakerOrHeadset()){
-            Toast.makeText(context, R.string.screen_audio_recording_not_allowed,Toast.LENGTH_LONG).show();
+        if (!isRoutedOnlyToSpeakerOrHeadset()) {
+            Toast.makeText(context, R.string.screen_audio_recording_not_allowed, Toast.LENGTH_LONG).show();
             return false;
         }
         return true;
@@ -93,18 +98,19 @@ public class Utils {
         return GlobalSettings.sRecordingStatus;
     }
 
-    public static void setStatus(UiStatus status) {
+    public static void setStatus(UiStatus status, Context context) {
         if (status.equals(UiStatus.SOUND)) {
-            setStatus(PREF_RECORDING_SOUND);
+            setStatus(PREF_RECORDING_SOUND, context);
         } else if (status.equals(UiStatus.SCREEN)) {
-            setStatus(PREF_RECORDING_SCREEN);
+            setStatus(PREF_RECORDING_SCREEN, context);
         } else {
-            setStatus(PREF_RECORDING_NOTHING);
+            setStatus(PREF_RECORDING_NOTHING, context);
         }
     }
 
-    public static void setStatus(String status) {
+    public static void setStatus(String status, Context context) {
         GlobalSettings.sRecordingStatus = status;
+        LocalBroadcastManager.getInstance(context).sendBroadcast(new Intent(Utils.ACTION_RECORDING_STATE_CHANGED));
     }
 
     public static boolean isRecording() {
@@ -136,6 +142,18 @@ public class Utils {
     private static int getDarkenedColorValue(int value) {
         float dark = 0.8f; // -20% lightness
         return Math.min(Math.round(value * dark), 255);
+    }
+
+    public static int getAudioRecordingType(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences(Utils.PREFS, 0);
+        return prefs.getInt(Utils.PREF_AUDIO_RECORDING_TYPE, Utils.PREF_AUDIO_RECORDING_TYPE_DEFAULT);
+    }
+
+    public static void stopOverlayService(Context context) {
+        // Stop overlay service if running
+        if (OverlayService.isRunning) {
+            context.stopService(new Intent(context, OverlayService.class));
+        }
     }
 
     /**
@@ -176,6 +194,10 @@ public class Utils {
         NOTHING,
         SOUND,
         SCREEN
+    }
+
+    public static void collapseStatusBar(Context context) {
+        context.sendBroadcast(new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS));
     }
 
 }