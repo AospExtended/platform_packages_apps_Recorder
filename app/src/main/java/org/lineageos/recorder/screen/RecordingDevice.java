PixelExperience/packages_apps_Recorder

Add system audio recording support [1/2]
Change-Id: Ie70ef75a2f833eefd49416b137d9da7ae7ae80d4

app/src/main/java/org/pixelexperience/recorder/screen/RecordingDevice.java
@@ -28,6 +28,8 @@ import android.media.MediaScannerConnection;
 import android.os.Environment;
 import android.util.Log;
 
+import org.pixelexperience.recorder.utils.Utils;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
@@ -41,12 +43,12 @@ class RecordingDevice extends EncoderDevice {
     private static final File RECORDINGS_DIR =
             new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES),
                     "ScreenRecords");
-    private final boolean mRecordAudio;
+    private final int mRecordAudioType;
     private final File mPath;
 
-    RecordingDevice(Context context, int width, int height, boolean recordAudio) {
+    RecordingDevice(Context context, int width, int height, int recordAudioType) {
         super(context, width, height);
-        mRecordAudio = recordAudio;
+        mRecordAudioType = recordAudioType;
         // Prepare all the output metadata
         String videoDate = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss", Locale.getDefault())
                 .format(new Date());
@@ -135,7 +137,8 @@ class RecordingDevice extends EncoderDevice {
         final MediaFormat format;
         MediaCodec codec;
 
-        AudioRecorder(Recorder recorder) {
+        AudioRecorder(Recorder recorder, int source) {
+            boolean isInternal = source == Utils.PREF_AUDIO_RECORDING_TYPE_INTERNAL;
             try {
                 codec = MediaCodec.createEncoderByType("audio/mp4a-latm");
             } catch (IOException e) {
@@ -143,9 +146,9 @@ class RecordingDevice extends EncoderDevice {
             }
             format = new MediaFormat();
             format.setString(MediaFormat.KEY_MIME, "audio/mp4a-latm");
-            format.setInteger(MediaFormat.KEY_BIT_RATE, 64 * 1024);
+            format.setInteger(MediaFormat.KEY_BIT_RATE, isInternal ? 156000 : 64 * 1024);
             format.setInteger(MediaFormat.KEY_CHANNEL_COUNT, 1);
-            format.setInteger(MediaFormat.KEY_SAMPLE_RATE, 44100);
+            format.setInteger(MediaFormat.KEY_SAMPLE_RATE, isInternal ? 48000 : 44100);
             format.setInteger(MediaFormat.KEY_AAC_PROFILE,
                     MediaCodecInfo.CodecProfileLevel.AACObjectHE);
             codec.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
@@ -154,13 +157,14 @@ class RecordingDevice extends EncoderDevice {
             this.recorder = recorder;
 
             int bufferSize = 1024 * 30;
-            int minBufferSize = AudioRecord.getMinBufferSize(44100, AudioFormat.CHANNEL_IN_MONO,
+            int minBufferSize = AudioRecord.getMinBufferSize(isInternal ? 48000 : 44100, AudioFormat.CHANNEL_IN_MONO,
                     AudioFormat.ENCODING_PCM_16BIT);
             if (bufferSize < minBufferSize) {
                 bufferSize = ((minBufferSize / 1024) + 1) * 1024 * 2;
             }
             Log.i(LOGTAG, "AudioRecorder init");
-            record = new AudioRecord(MediaRecorder.AudioSource.MIC, 44100,
+            record = new AudioRecord(isInternal ? MediaRecorder.AudioSource.REMOTE_SUBMIX :
+                    MediaRecorder.AudioSource.MIC, isInternal ? 48000 : 44100,
                     AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, bufferSize);
         }
 
@@ -270,8 +274,8 @@ class RecordingDevice extends EncoderDevice {
 
                     // now that we have the Magic Goodies, start the muxer
                     trackIndex = muxer.addTrack(newFormat);
-                    if (mRecordAudio) {
-                        audio = new AudioRecorder(this);
+                    if (mRecordAudioType != Utils.PREF_AUDIO_RECORDING_TYPE_DISABLED) {
+                        audio = new AudioRecorder(this, mRecordAudioType);
                         Semaphore semaphore = new Semaphore(0);
                         audioMuxer = new AudioMuxer(audio, muxer, semaphore);
                         muxerStarted = true;

app/src/main/java/org/pixelexperience/recorder/utils/Utils.java
@@ -15,10 +15,16 @@
  */
 package org.pixelexperience.recorder.utils;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothProfile;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.media.AudioSystem;
 import android.util.DisplayMetrics;
+import android.widget.Toast;
+
+import org.pixelexperience.recorder.R;
 
 import java.io.Closeable;
 import java.io.IOException;
@@ -29,9 +35,58 @@ public class Utils {
     public static final String PREF_RECORDING_NOTHING = "nothing";
     public static final String PREF_RECORDING_SCREEN = "screen";
     private static final String PREF_RECORDING_SOUND = "sound";
-    public static final String PREF_SCREEN_WITH_AUDIO = "screen_with_audio";
+    public static final String PREF_AUDIO_RECORDING_TYPE = "audio_recording_type";
+    public static final int PREF_AUDIO_RECORDING_TYPE_DISABLED = 0;
+    public static final int PREF_AUDIO_RECORDING_TYPE_INTERNAL = 1;
+    public static final int PREF_AUDIO_RECORDING_TYPE_MICROPHONE = 2;
+    public static final int PREF_AUDIO_RECORDING_TYPE_DEFAULT = PREF_AUDIO_RECORDING_TYPE_DISABLED;
 
     private Utils() {
+    }
+
+    public static boolean isWifiDisplaySessionRunning(){
+        return (AudioSystem.getDevicesForStream(AudioSystem.STREAM_MUSIC) & AudioSystem.DEVICE_OUT_PROXY) != 0;
+    }
+
+    public static boolean isRoutedToSubmix(){
+        return (AudioSystem.getDevicesForStream(AudioSystem.STREAM_MUSIC) & AudioSystem.DEVICE_OUT_REMOTE_SUBMIX) != 0;
+    }
+
+    public static boolean isBluetoothHeadsetConnected() {
+        BluetoothAdapter ba = BluetoothAdapter.getDefaultAdapter();
+        if (ba == null || !ba.isEnabled()) {
+            return false;
+        }
+        int a2dp = ba.getProfileConnectionState(BluetoothProfile.A2DP);
+        int headset = ba.getProfileConnectionState(BluetoothProfile.HEADSET);
+        return a2dp == BluetoothProfile.STATE_CONNECTED || headset == BluetoothProfile.STATE_CONNECTED;
+    }
+
+    public static boolean isRoutedOnlyToSpeakerOrHeadset(){
+        if (isBluetoothHeadsetConnected()){
+            return false;
+        }
+        int devices = AudioSystem.getDevicesForStream(AudioSystem.STREAM_MUSIC);
+        devices &= ~AudioSystem.DEVICE_OUT_REMOTE_SUBMIX; // Remove submix
+        return (devices == AudioSystem.DEVICE_OUT_SPEAKER || devices == AudioSystem.DEVICE_OUT_SPEAKER_SAFE ||
+                devices == AudioSystem.DEVICE_OUT_WIRED_HEADPHONE || devices == AudioSystem.DEVICE_OUT_WIRED_HEADSET ||
+                devices == AudioSystem.DEVICE_OUT_USB_HEADSET);
+    }
+
+    public static boolean isInternalAudioRecordingAllowed(Context context, boolean checkSubmix){
+        if (isWifiDisplaySessionRunning()){
+            Toast.makeText(context, R.string.screen_audio_recording_disabled_wfd,Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (checkSubmix && isRoutedToSubmix()){
+            Toast.makeText(context, R.string.screen_audio_recording_disabled_others_apps,Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (!isRoutedOnlyToSpeakerOrHeadset()){
+            Toast.makeText(context, R.string.screen_audio_recording_not_allowed,Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
     }
 
     private static String getStatus(Context context) {

AospExtended/platform_packages_apps_Recorder

Signed-off-by: DennySPB <dennyspb@gmail.com>
Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

app/src/main/java/org/lineageos/recorder/screen/RecordingDevice.java
@@ -28,6 +28,8 @@ import android.media.MediaScannerConnection;
 import android.os.Environment;
 import android.util.Log;
 
+import org.lineageos.recorder.utils.Utils;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
@@ -41,12 +43,12 @@ class RecordingDevice extends EncoderDevice {
     private static final File RECORDINGS_DIR =
             new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES),
                     "ScreenRecords");
-    private final boolean mRecordAudio;
+    private final int mRecordAudioType;
     private final File mPath;
 
-    RecordingDevice(Context context, int width, int height, boolean recordAudio) {
+    RecordingDevice(Context context, int width, int height, int recordAudioType) {
         super(context, width, height);
-        mRecordAudio = recordAudio;
+        mRecordAudioType = recordAudioType;
         // Prepare all the output metadata
         String videoDate = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss", Locale.getDefault())
                 .format(new Date());
@@ -135,7 +137,8 @@ class RecordingDevice extends EncoderDevice {
         final MediaFormat format;
         MediaCodec codec;
 
-        AudioRecorder(Recorder recorder) {
+        AudioRecorder(Recorder recorder, int source) {
+            boolean isInternal = source == Utils.PREF_AUDIO_RECORDING_TYPE_INTERNAL;
             try {
                 codec = MediaCodec.createEncoderByType("audio/mp4a-latm");
             } catch (IOException e) {
@@ -143,9 +146,9 @@ class RecordingDevice extends EncoderDevice {
             }
             format = new MediaFormat();
             format.setString(MediaFormat.KEY_MIME, "audio/mp4a-latm");
-            format.setInteger(MediaFormat.KEY_BIT_RATE, 64 * 1024);
+            format.setInteger(MediaFormat.KEY_BIT_RATE, isInternal ? 156000 : 64 * 1024);
             format.setInteger(MediaFormat.KEY_CHANNEL_COUNT, 1);
-            format.setInteger(MediaFormat.KEY_SAMPLE_RATE, 44100);
+            format.setInteger(MediaFormat.KEY_SAMPLE_RATE, isInternal ? 48000 : 44100);
             format.setInteger(MediaFormat.KEY_AAC_PROFILE,
                     MediaCodecInfo.CodecProfileLevel.AACObjectHE);
             codec.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
@@ -154,13 +157,14 @@ class RecordingDevice extends EncoderDevice {
             this.recorder = recorder;
 
             int bufferSize = 1024 * 30;
-            int minBufferSize = AudioRecord.getMinBufferSize(44100, AudioFormat.CHANNEL_IN_MONO,
+            int minBufferSize = AudioRecord.getMinBufferSize(isInternal ? 48000 : 44100, AudioFormat.CHANNEL_IN_MONO,
                     AudioFormat.ENCODING_PCM_16BIT);
             if (bufferSize < minBufferSize) {
                 bufferSize = ((minBufferSize / 1024) + 1) * 1024 * 2;
             }
             Log.i(LOGTAG, "AudioRecorder init");
-            record = new AudioRecord(MediaRecorder.AudioSource.MIC, 44100,
+            record = new AudioRecord(isInternal ? MediaRecorder.AudioSource.REMOTE_SUBMIX :
+                    MediaRecorder.AudioSource.MIC, isInternal ? 48000 : 44100,
                     AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, bufferSize);
         }
 
@@ -270,8 +274,8 @@ class RecordingDevice extends EncoderDevice {
 
                     // now that we have the Magic Goodies, start the muxer
                     trackIndex = muxer.addTrack(newFormat);
-                    if (mRecordAudio) {
-                        audio = new AudioRecorder(this);
+                    if (mRecordAudioType != Utils.PREF_AUDIO_RECORDING_TYPE_DISABLED) {
+                        audio = new AudioRecorder(this, mRecordAudioType);
                         Semaphore semaphore = new Semaphore(0);
                         audioMuxer = new AudioMuxer(audio, muxer, semaphore);
                         muxerStarted = true;

app/src/main/java/org/lineageos/recorder/utils/Utils.java
@@ -15,10 +15,16 @@
  */
 package org.lineageos.recorder.utils;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothProfile;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.media.AudioSystem;
 import android.util.DisplayMetrics;
+import android.widget.Toast;
+
+import org.lineageos.recorder.R;
 
 import java.io.Closeable;
 import java.io.IOException;
@@ -29,9 +35,58 @@ public class Utils {
     public static final String PREF_RECORDING_NOTHING = "nothing";
     public static final String PREF_RECORDING_SCREEN = "screen";
     private static final String PREF_RECORDING_SOUND = "sound";
-    public static final String PREF_SCREEN_WITH_AUDIO = "screen_with_audio";
+    public static final String PREF_AUDIO_RECORDING_TYPE = "audio_recording_type";
+    public static final int PREF_AUDIO_RECORDING_TYPE_DISABLED = 0;
+    public static final int PREF_AUDIO_RECORDING_TYPE_INTERNAL = 1;
+    public static final int PREF_AUDIO_RECORDING_TYPE_MICROPHONE = 2;
+    public static final int PREF_AUDIO_RECORDING_TYPE_DEFAULT = PREF_AUDIO_RECORDING_TYPE_DISABLED;
 
     private Utils() {
+    }
+
+    public static boolean isWifiDisplaySessionRunning(){
+        return (AudioSystem.getDevicesForStream(AudioSystem.STREAM_MUSIC) & AudioSystem.DEVICE_OUT_PROXY) != 0;
+    }
+
+    public static boolean isRoutedToSubmix(){
+        return (AudioSystem.getDevicesForStream(AudioSystem.STREAM_MUSIC) & AudioSystem.DEVICE_OUT_REMOTE_SUBMIX) != 0;
+    }
+
+    public static boolean isBluetoothHeadsetConnected() {
+        BluetoothAdapter ba = BluetoothAdapter.getDefaultAdapter();
+        if (ba == null || !ba.isEnabled()) {
+            return false;
+        }
+        int a2dp = ba.getProfileConnectionState(BluetoothProfile.A2DP);
+        int headset = ba.getProfileConnectionState(BluetoothProfile.HEADSET);
+        return a2dp == BluetoothProfile.STATE_CONNECTED || headset == BluetoothProfile.STATE_CONNECTED;
+    }
+
+    public static boolean isRoutedOnlyToSpeakerOrHeadset(){
+        if (isBluetoothHeadsetConnected()){
+            return false;
+        }
+        int devices = AudioSystem.getDevicesForStream(AudioSystem.STREAM_MUSIC);
+        devices &= ~AudioSystem.DEVICE_OUT_REMOTE_SUBMIX; // Remove submix
+        return (devices == AudioSystem.DEVICE_OUT_SPEAKER || devices == AudioSystem.DEVICE_OUT_SPEAKER_SAFE ||
+                devices == AudioSystem.DEVICE_OUT_WIRED_HEADPHONE || devices == AudioSystem.DEVICE_OUT_WIRED_HEADSET ||
+                devices == AudioSystem.DEVICE_OUT_USB_HEADSET);
+    }
+
+    public static boolean isInternalAudioRecordingAllowed(Context context, boolean checkSubmix){
+        if (isWifiDisplaySessionRunning()){
+            Toast.makeText(context, R.string.screen_audio_recording_disabled_wfd,Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (checkSubmix && isRoutedToSubmix()){
+            Toast.makeText(context, R.string.screen_audio_recording_disabled_others_apps,Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (!isRoutedOnlyToSpeakerOrHeadset()){
+            Toast.makeText(context, R.string.screen_audio_recording_not_allowed,Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
     }
 
     private static String getStatus(Context context) {